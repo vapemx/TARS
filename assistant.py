7715fd39d1ac80894c7c6246390187d9ead4fcdf
vapemx <vapemx@tutanota.com>
2021-04-07 22:12:36 -0500

    Se anade la logica principal Logica principal, reconocimiento del nombre, readme, requeriments Need to test and make prettier

--- assistant.py
+++ assistant.py
@@ -1,7 +1,34 @@
 import pyttsx3
+import speech_recognition as sr
+import re
 
-engine = pyttsx3.init()
-engine.setProperty("voice", "spanish")
 
-engine.say("Hola, esto es una prueba")
-engine.runAndWait()
+def main():
+    engine = pyttsx3.init()
+    engine.setProperty("voice", "spanish")
+    engine.say("Hola, esto es una prueba, ¿Cómo te llamas?")
+    engine.runAndWait()
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Puedes hablar...")
+        audio = r.listen(source)
+        text = r.recognize_google(audio, language="es-MX")
+    user_name = know_user_name(text, engine)
+
+def know_user_name(text, engine):
+    user_name = None
+    patterns = ["me llamo ([A-Za-z]+)", "mi nombre es ([A-Za-z]+)", "^([A-Za-z]+)$"]
+    for pattern in patterns:
+        try:
+            user_name = re.findall(pattern, text)
+            print(user_name[0])
+            engine.say("Hola, {} espero tengas un buen día".format(user_name[0]))
+            engine.runAndWait()
+            return user_name[0]
+        except IndexError:
+            print("Nombre no detectado")
+            engine.say("Lo siento, no he escuchado tu nombre")
+            engine.runAndWait()
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
+# TARS
+This is a virtual assintant. 
+Now in progress.
+
 ## Installation
+- Linux
 `sudo apt install espeak`
 `pip install requeriments.txt`
+- Windows
+  `pip install requeriments_win.txt`
